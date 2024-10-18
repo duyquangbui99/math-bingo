86fdd7449448a5e533f5cbd32151767096291696
Quang Bui <77463586+duyquangbui99@users.noreply.github.com>
2024-10-17 20:45:13 -0500

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-/* Randomize array in-place using Durstenfeld shuffle algorithm */
+/* Randomize array in-place using durstenfeld shuffle algorithm */
 function shuffleArray(array) {
   for (var i = array.length - 1; i > 0; i--) {
     var j = Math.floor(Math.random() * (i + 1));
